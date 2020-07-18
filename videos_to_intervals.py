358312b6e493aab0e2a4dae6f0f8cfe345547557
tom <tomgoter@gmail.com>
2020-07-18 11:33:52 -0400

    changed video_to_intervals to allow for modifying framerate

--- videos_to_intervals.py
+++ videos_to_intervals.py
@@ -15,6 +15,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--video_root", help="Root folder of raw videos", required=True)
 parser.add_argument("--interval_root", help="where to create the interval files", required=True)
+parser.add_argument("--fps", help="optional parameter to allow you to change the fps of the output video")
 
 args = parser.parse_args()
 
@@ -22,14 +23,20 @@ video_list = [vid for vid in os.listdir(args.video_root) if '.mov' == vid[-4:]]
 print("Total Number of Videos to Process {}".format(len(video_list)))
 
 # Template string to run ffmpeg over and over with
-template = 'ffmpeg -i {}/{} -c copy -segment_time 30 -f segment {}/{}/cut-%d.mov'
+if args.fps:
+  template = 'ffmpeg -i {}/{} -c copy -segment_time 30 fps=fps={} -f segment {}/{}/cut-%d.mov'
+else:
+  template = 'ffmpeg -i {}/{} -c copy -segment_time 30 -f segment {}/{}/cut-%d.mov'
 
 # Loop over videos and chunk
 for vid in tqdm(video_list):
   base_path = vid[:-4]
   if not os.path.isdir(os.path.join(args.interval_root, base_path)):  
     os.mkdir(os.path.join(args.interval_root, base_path))
-    os.system(template.format(args.video_root, vid, args.interval_root, base_path))
+    if args.fps:
+      os.system(template.format(args.video_root, vid, args.fps, args.interval_root, base_path))
+    else:
+      os.system(template.format(args.video_root, vid, args.interval_root, base_path))
   else:
     continue
     

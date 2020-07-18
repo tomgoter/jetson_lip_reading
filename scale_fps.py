a4ae0d55e28b2e14cdc9b10814cc9d60de389f4c
root <root@cpu48.w251.cloud>
2020-07-18 21:38:37 +0000

    another small fix to correct file names

--- scale_fps.py
+++ scale_fps.py
@@ -2,16 +2,16 @@
 import sys, os, argparse
 from glob import glob
 import concurrent.futures
-
+import numpy as np
 
 
 def process_video(video):
+    #
+    print('Working on Video File: {}'.format(video))
     # Create a list of images to downsample
     cut_list = os.listdir(os.path.join(args.input_dir,video))
-    print(cut_list)
+   
     for cut in cut_list:
-        # Where are we
-        print('Working on Video File: {}'.format(cut))
 
         # Make output directory
         os.makedirs(os.path.join(args.output_dir,video,cut), exist_ok=True)
@@ -31,33 +31,35 @@ def process_video(video):
         print("Removing every {} image".format(stopper))
         for id in sorted_ids:
             if id != 0 and (id+1) % stopper == 0:
-                counter  = (id+1) / stopper
+                counter  = int((id+1) / stopper)
                 continue
             else:
                 os.system('cp {0}/{1}/{2}/{3}.jpg {4}/{1}/{2}/{5}.jpg'.format(
-                    args.input_idr, video, cut, id, args.output_dir, (id-counter)))
+                    args.input_dir, video, cut, id, args.output_dir, (id-counter)))
                 #os.system(f'cp {args.input_dir}/{video}/{cut}/{id}.jpg {args.output_dir}/{video}/{cut}/{id-counter}.jpg')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', "--input_dir", help="Path to images to edit", required=True)
     parser.add_argument('-o', "--output_dir", help="Path to images to edit", required=True)
-    parser.add_argument('-n', "--num_cpus", help="Number of CPUs to use", default=1)
-    parser.add_argument('--fps', type=int, choices=[10, 15, 20], help="Desired Output FPS", required=True)
+    parser.add_argument('-n', "--num_cpus",type=int, help="Number of CPUs to use", default=1)
+    parser.add_argument('--fps', type=float, choices=[10, 15, 20], help="Desired Output FPS", required=True)
     args = parser.parse_args()
 
     # Original Videos recorded at 30 FPS
     BASE_FPS = 30
 
     # Identify how frequently to remove a frame
-    stopper = int(1 / (1 - (args.fps / BASE_FPS)))
-
+    stopper = np.round(1 / (1 - (args.fps / BASE_FPS)),0)
+    print('Stopper is {}'.format(stopper))
     # Make Output Director
     os.makedirs(args.output_dir, exist_ok=True)
 
     #
     videos = os.listdir(args.input_dir)
-
+    
+    #
+    print("Modifying the preprocessed images from the following videos: {}".format(videos))
     with concurrent.futures.ProcessPoolExecutor(args.num_cpus) as executor:
         result = executor.map(process_video, videos)
 

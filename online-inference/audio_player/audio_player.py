c4c943f1e15e706e7066fe863f7d4cf40f21d22a
Diana Iftimie <dianai@ischool.berkeley.edu>
2020-07-26 14:28:15 -0700

    updated audio player to get closer to streaming

--- online-inference/audio_player/audio_player.py
+++ online-inference/audio_player/audio_player.py
@@ -17,10 +17,10 @@ from scipy.io import wavfile
 
 # https://stackoverflow.com/questions/43941716/how-to-play-mp3-from-bytes
 
-from pydub import AudioSegment
-from pydub.playback import play
+#from pydub import AudioSegment
+#from pydub.playback import play
 
-from pygame import mixer, time
+#from pygame import mixer, time
 
 # Subscribing client params
 SUBSCRIBING_CLIENT_NAME = str(sys.argv[1])
@@ -30,7 +30,8 @@ SUBSCRIBING_QOS = int(sys.argv[4])
 SUBSCRIBE_TO_TOPIC = str(sys.argv[5])
 
 # make a queue of wav files to play
-wav_queue = queue.Queue()
+wav_bytes_queue = queue.Queue()
+wav_files_queue = queue.Queue()
 
 def on_log(client, userdata, level, buf):
    print(buf)
@@ -48,6 +49,12 @@ def on_disconnect(client, userdata, rc):
 def on_subscribe(client, userdata, mid, granted_qos):
    print("subscribed")   
 
+def convert_wav_bytes_to_wav_file(wav_bytes, wav_num):
+   rate, data = wavfile.read(io.BytesIO(wav_bytes))
+   outfile = save_wav(data, rate, wav_num)
+   wav_files_queue.put(outfile, block=True)
+
+wav_num = 0
 def on_message(client, userdata, message):
    print("message topic=", message.topic)
    print("message qos=", message.qos)
@@ -55,7 +62,10 @@ def on_message(client, userdata, message):
    print("\n")
 
    # queue up message to be played
-   wav_queue.put(message.payload, block=True)
+   #wav_bytes_queue.put(message.payload, block=True)
+   global wav_num
+   convert_wav_bytes_to_wav_file(message.payload, wav_num)
+   wav_num += 1
 
    
 # Set up client & callbacks
@@ -79,37 +89,52 @@ def save_wav(byte_data, rate, wav_num):
    wavfile.write(outfile, rate, byte_data.astype(np.int16))
    return outfile
 
+def process_wav_bytes(wav_bytes, wav_num):
+   rate, data = wavfile.read(io.BytesIO(wav_bytes))
+   
+   # saving the wav file and playing -- works great, just theoretically slower
+   outfile = save_wav(data, rate, wav_num)
+   playsound(outfile)
+      
 
+   '''
+   # pydub -- had issues with ffprobe errors
+   song = AudioSegment.from_file(io.BytesIO(wav_bytes), format="wav")
+   play(song)
+   '''
+
+   '''
+   # pygame -- sounds like crap...
+   sound = mixer.Sound(wav_bytes)
+   audio = sound.play()
+   while audio.get_busy():
+      time.Clock().tick(10)
+   '''
+
+   '''
+   # soundfile -- can't get import to work
+   data, samplerate = sf.read(io.BytesIO(wav_bytes))
+   '''
+
+
+'''
 # Wait for messages until disconnected by system interrupt
 wav_num = 0
 while True:
-   if (wav_queue.qsize() > 0):
-      wav_bytes = wav_queue.get(block=True)
-      rate, data = wavfile.read(io.BytesIO(wav_bytes))
-
-      
-      # saving the wav file and playing -- works great, just theoretically slower
-      outfile = save_wav(data, rate, wav_num)
-      playsound(outfile)
-      
-
-      '''
-      # pydub -- had issues with ffprobe errors
-      song = AudioSegment.from_file(io.BytesIO(wav_bytes), format="wav")
-      play(song)
-      '''
-
-      '''
-      # pygame -- sounds like crap...
-      sound = mixer.Sound(wav_bytes)
-      audio = sound.play()
-      while audio.get_busy():
-         time.Clock().tick(10)
-      '''
-
-      '''
-      # soundfile -- can't get import to work
-      data, samplerate = sf.read(io.BytesIO(wav_bytes))
-      '''
-
+   if (wav_bytes_queue.qsize() > 0):
+      # case: queuing wav bytes
+      #process_wav_bytes(wav_bytes_queue.get(block=True), wav_num)
       wav_num += 1
+'''
+
+# Wait for messages until disconnected by system interrupt
+while True:
+   if (wav_files_queue.qsize() > 0):
+      # case: queuing file paths to saved wav files
+      playsound(wav_files_queue.get(block=True))
+
+
+
+
+
+
